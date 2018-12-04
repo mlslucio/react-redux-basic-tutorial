d506f2b29500bbb35b9b9a046b80860e96fbce21
mauro silva <mauro.mls@hotmail.com>
2018-12-04 17:02:38 -0200

    concluido

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PostForm from './PostForm';
+import AllPost from './AllPost';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <PostForm />
+          <AllPost />
+      </div>
+    );
+  }
+}
+
+export default App;
